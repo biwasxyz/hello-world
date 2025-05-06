eec06102de2ec27b624d797a2d5751e88253b2f6
biwasbhandari <biwas2059@gmail.com>
2025-05-06 09:33:32 +0545

    Testing

--- hello-world/ContentView.swift
+++ hello-world/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Biwas!")
+            Text("Hello, Biwas test!")
         }
         .padding()
     }
