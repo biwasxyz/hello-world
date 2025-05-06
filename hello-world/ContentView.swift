9bb6df01e9482dc7c20bd9ccdb56c050116fcbce
biwasbhandari <biwas2059@gmail.com>
2025-05-06 10:18:19 +0545

    fix: initialize social media app

--- hello-world/ContentView.swift
+++ hello-world/ContentView.swift
@@ -10,13 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Github!")
+            Text("AppleMedia")
+                .font(.title)
+                .padding()
+                .bold()
+                .foregroundColor(.blue)
         }
-        .padding()
+        
+        TextField("What's on your mind?", text: .constant(""))
+            .foregroundColor(.black)
+            .padding()
+        Spacer()
+        
     }
+   
+    
 }
 
 #Preview {
